03673dbded852e80063046da4f218f97e395f8a8
Alexandr Chernyaev <bliz48rus@gmail.com>
2021-01-24 17:35:40 +0300

    Update checked.php

--- checked.php
+++ checked.php
@@ -9,7 +9,7 @@ class Update
     public $packages = [
         [
             'name'    => 'sajya/server',
-            'version' => '2.0.0.0',
+            'version' => '2.2.0.0',
         ],
         [
             'name'    => 'orchid/experiment',
